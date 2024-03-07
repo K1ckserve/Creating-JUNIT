f3148654060989dc2ae8cda7e0dfc00008bb0601
K1ckserve <bendalton6463@gmail.com>
2024-03-07 14:30:23 -0600

    Added filtered test config

--- unittest/driver/TestDriver.java
+++ unittest/driver/TestDriver.java
@@ -3,9 +3,14 @@ package unittest.driver;
 import sampletest.TestA;
 import unittest.results.TestClassResult;
 import unittest.results.TestMethodResult;
+import unittest.runners.FilteredTestRunner;
+import unittest.runners.OrderedTestRunner;
+import unittest.runners.ParameterizedTestRunner;
+import unittest.runners.TestRunner;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TestDriver {
@@ -20,9 +25,30 @@ public class TestDriver {
         ArrayList<TestClassResult> results = new ArrayList<TestClassResult>();
         for (String className : testclasses) {
             try {
-                Class<?> clazz = Class.forName(className);
-                Object testInstance = clazz.getDeclaredConstructor().newInstance(); // cut it right here and put it in test runner
-                TestClassResult classResult = new TestClassResult(className); //we will just use the test runners run
+                Class<?> clazz; // put all of these are becasue the scop was fucked
+                Object testInstance;
+                FilteredTestRunner FTR;
+                OrderedTestRunner ORT;
+                ParameterizedTestRunner PTR;
+                TestRunner TR;
+                if(className.contains("#")){ // this will correctly return all of the tests for the filitred test method
+                    ArrayList<String> mthds = new ArrayList<>();
+                    String[] methodsToRun = null;
+                    String[] parts = className.split("#");
+                    className = parts[0];
+                    methodsToRun = parts[1].split(",");
+                    Collections.addAll(mthds, methodsToRun);
+                    clazz = Class.forName(className);
+                    testInstance = clazz.getDeclaredConstructor().newInstance();
+                    FTR = new FilteredTestRunner(clazz, mthds);
+                }else{ // if nothing else then we will run basic TR
+                    clazz = Class.forName(className);
+                    testInstance = clazz.getDeclaredConstructor().newInstance(); // cut it right here and put it in test runner
+                    TR = new TestRunner(clazz);
+                }
+//                Class<?> clazz = Class.forName(className);
+//                Object testInstance = clazz.getDeclaredConstructor().newInstance(); // cut it right here and put it in test runner
+//                TestClassResult classResult = new TestClassResult(className); //we will just use the test runners run
 
                 for (Method method : clazz.getDeclaredMethods()) {
                     if (method.isAnnotationPresent(unittest.annotations.Test.class)) {
@@ -49,7 +75,7 @@ public class TestDriver {
 
     public static void main(String[] args) {
         // Use this for your testing.  We will not be calling this method.
-        String[] testClasses = {"sampletest.TestA"};
+        String[] testClasses = {"sampletest.TestA#test1"};
         runTests(testClasses);
     }
 }
