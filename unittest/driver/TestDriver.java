01959208e072b593e69d7695a03e9b5679c88c15
K1ckserve <bendalton6463@gmail.com>
2024-03-18 12:47:08 -0500

    test

--- unittest/driver/TestDriver.java
+++ unittest/driver/TestDriver.java
@@ -89,7 +89,7 @@ public class TestDriver {
     public static void main(String[] args) {
         // Use this for your testing.  We will not be calling this method.
         //String[] testClasses = {"sampletest.TestC#test4,test5"};
-        String[] testClasses = {"sampletest.TestD"};
+        String[] testClasses = {"sampletest.TestC"};
         runTests(testClasses);
     }
 }
