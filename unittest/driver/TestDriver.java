50c15efdceea3f6b8128703ca88616804662cdd1
K1ckserve <bendalton6463@gmail.com>
2024-03-29 17:23:39 -0500

    end of friday

--- unittest/driver/TestDriver.java
+++ unittest/driver/TestDriver.java
@@ -18,13 +18,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class TestDriver {
+    String[] test;
+    public TestDriver(String[] test){
+        this.test =test;
+    }
 
 
     /**
      * Execute the specified test classes, returning the results of running
      * each test class, in the order given.
      */
-    public static List<TestClassResult> runTests(String[] testclasses) {
+    public List<TestClassResult> runTests(String[] testclasses) {
         int flag=0;
         // TODO: complete this method
         // We will call this method from our JUnit test cases.
@@ -97,60 +101,11 @@ public class TestDriver {
             System.out.println(print.get(s).getTestClassName()+"."+s.getName()+":");
             s.getException().printStackTrace();
         }
-//        for(Map.Entry<TestMethodResult, TestClassResult> iter: print.entrySet()){
-//            TestMethodResult key = iter.getKey();
-//            TestClassResult value = iter.getValue();
-//            System.out.println(value.getTestClassName()+"."+key.getName()+":");
-//            key.getException().printStackTrace();
-//        }
         System.out.println("==========");
         System.out.println("Tests run: "+ counter+", Failures: "+print.size());
     }
 
-//    public static void printNormal(ArrayList<TestClassResult> results){
-//        int counter = 0;
-//        Map<TestMethodResult, TestClassResult> print = new LinkedHashMap<>(); // Use LinkedHashMap to preserve insertion order after sorting
-//
-//        for (TestClassResult a : results) {
-//            // Sort test method results by method name
-//            List<TestMethodResult> sortedMethodResults = a.getTestMethodResults().stream()
-//                    .sorted(Comparator.comparing(TestMethodResult::getName))
-//                    .collect(Collectors.toList());
-//
-//            for (TestMethodResult b : sortedMethodResults) {
-//                String outcome = b.isPass() ? "PASS" : "FAIL";
-//                if (!b.isPass()) {
-//                    print.put(b, a); // Assuming failure details are to be printed later
-//                }
-//                System.out.println(a.getTestClassName() + "." + b.getName() + " : " + outcome);
-//                counter++;
-//            }
-//        }
-//
-//        System.out.println("==========");
-//        System.out.println("FAILURES:");
-//        for (Map.Entry<TestMethodResult, TestClassResult> entry : print.entrySet()) {
-//            TestMethodResult testMethodResult = entry.getKey();
-//            TestClassResult testClassResult = entry.getValue();
-//
-//            // Print the class and method name for the failing test
-//            System.out.println(testClassResult.getTestClassName() + "." + testMethodResult.getName() + ":");
-//
-//            // Assuming your TestMethodResult has a getException() method that returns an AssertionException
-//            AssertionException exception = testMethodResult.getException();
-//
-//            // Check if there's an exception and it's an instance of AssertionException
-//            if (exception != null) {
-//                // Print the assertion error message and custom stack trace
-//                // Print the message
-//                exception.printStackTrace(); // Print the custom stack trace
-//            }
-//        }
-//        System.out.println("==========");
-//        System.out.println("Tests run: " + counter + ", Failures: " + print.size());
-//    }
-//ff
-    public static void main(String[] args) {
+    public void main(String[] args) {
         // Use this for your testing.  We will not be calling this method.
         String[] testClasses = {"studenttest.test1#testB,testA","studenttest.test2"};
         runTests(testClasses);
--- unittest/gui/TestGUI.java
+++ unittest/gui/TestGUI.java
@@ -3,6 +3,7 @@ package unittest.gui;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -10,10 +11,15 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import unittest.annotations.Test;
+import unittest.driver.TestDriver;
+import unittest.runners.FilteredTestRunner;
+import unittest.runners.TestRunner;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestGUI extends Application {
 
@@ -34,12 +40,16 @@ public class TestGUI extends Application {
 
     @Override
     public void start(Stage applicationStage) throws Exception {
+        Map<String,ArrayList<String>> d = new HashMap<>();
+        d.put("test1",new ArrayList<>());
+        d.put("test2",new ArrayList<>());
+        d.put("test3",new ArrayList<>());
         applicationStage.setTitle("Test Framework GUI");
 
         GridPane gridPane = new GridPane();
         gridPane.setHgap(10);
         gridPane.setVgap(10);
-        gridPane.setPadding(new javafx.geometry.Insets(20, 20, 20, 20));
+        gridPane.setPadding(new Insets(20, 20, 20, 20));
 
         ListView<CheckBox> testSelectionListView = new ListView<>();
         ObservableList<CheckBox> testItems = FXCollections.observableArrayList();
@@ -47,7 +57,7 @@ public class TestGUI extends Application {
         for (Class<?> testClass : getTestClasses()) {
             for (Method method : testClass.getDeclaredMethods()) {
                 if (method.isAnnotationPresent(Test.class)) { // Check if the method is annotated with @Test
-                    CheckBox checkBox = new CheckBox(testClass.getSimpleName() + " - " + method.getName());
+                    CheckBox checkBox = new CheckBox(testClass.getSimpleName() + "-" + method.getName());
                     testItems.add(checkBox);
                 }
             }
@@ -61,9 +71,39 @@ public class TestGUI extends Application {
             for (CheckBox checkBox : testItems) {
                 if (checkBox.isSelected()) {
                     System.out.println("Selected: " + checkBox.getText());
+
+
+                    String[] theClass = checkBox.getText().split("-");
+                    d.get(theClass[0]).add(theClass[1]);
+//                    try{
+//                        String[] testD;
+//                        Class<?> cls = Class.forName("studenttest." +theClass[0]+"."+theClass[1]);
+////                        String name = theClass[1];
+// //                       List<String> testsToTest = new ArrayList<>();
+////                        testsToTest.add(name);
+////                        TestRunner filt = new FilteredTestRunner(cls,testsToTest,theClass[0]);
+//                    }catch(ClassNotFoundException w){
+//
+//                    }
                     // Place your logic to run the test here.
                 }
             }
+            int count =0;
+            ArrayList<String> testDriverSend = new ArrayList<>();
+            for(String key : d.keySet()){
+                testDriverSend.add("studenttest."+key+"#");
+                for(String l : d.get(key)){
+                    testDriverSend.set(count, testDriverSend.get(count) +l+",");
+                }
+                testDriverSend.set(count, testDriverSend.get(count).substring(0, testDriverSend.get(count).length()-1));
+                count++;
+            }
+            String[] testMethods = new String[testDriverSend.size()];
+            for (int i = 0; i < testDriverSend.size(); i++) {
+                testMethods[i] = testDriverSend.get(i);
+            }
+            TestDriver run = new TestDriver(testMethods);
+            run.runTests(testMethods);
         });
 
         gridPane.add(runTestsButton, 0, 1, 2, 1);
