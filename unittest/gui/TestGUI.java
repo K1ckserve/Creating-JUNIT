5fa04875be7461dd0d56bed9e1927a4625933c78
simon <simonyarmo@gmail.com>
2024-03-29 16:15:20 -0500

    Update TestGUI.java

--- unittest/gui/TestGUI.java
+++ unittest/gui/TestGUI.java
@@ -1,52 +1,79 @@
 package unittest.gui;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import unittest.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestGUI extends Application {
 
+    // Assuming these are your test classes
+    private List<Class<?>> getTestClasses() {
+        List<Class<?>> classes = new ArrayList<>();
+        // Dynamically adding classes would require a classpath scanning library
+        try {
+            // Mock-up example, replace with actual class names
+            classes.add(Class.forName("studenttest.test1"));
+            classes.add(Class.forName("studenttest.test2"));
+            classes.add(Class.forName("studenttest.test3"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return classes;
+    }
+
     @Override
-    public void start(Stage applicationStage) {
+    public void start(Stage applicationStage) throws Exception {
         applicationStage.setTitle("Test Framework GUI");
 
-        // GridPane layout
         GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setHgap(10); // Horizontal gap between cells
-        gridPane.setVgap(10); // Vertical gap between cells
-        gridPane.setPadding(new Insets(25, 25, 25, 25)); // Padding around the grid (top, right, bottom, left)
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new javafx.geometry.Insets(20, 20, 20, 20));
 
-        // Test selection label
-        Label testSelectionLabel = new Label("Select Tests to Run:");
-        gridPane.add(testSelectionLabel, 0, 0, 2, 1); // Add to column=0, row=0, colspan=2, rowspan=1
+        ListView<CheckBox> testSelectionListView = new ListView<>();
+        ObservableList<CheckBox> testItems = FXCollections.observableArrayList();
 
-        // Run button
+        for (Class<?> testClass : getTestClasses()) {
+            for (Method method : testClass.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(Test.class)) { // Check if the method is annotated with @Test
+                    CheckBox checkBox = new CheckBox(testClass.getSimpleName() + " - " + method.getName());
+                    testItems.add(checkBox);
+                }
+            }
+        }
+
+        testSelectionListView.setItems(testItems);
+        gridPane.add(testSelectionListView, 0, 0, 2, 1);
+
         Button runTestsButton = new Button("Run Selected Tests");
         runTestsButton.setOnAction(e -> {
-            // Logic to run tests goes here
-            System.out.println("Running tests...");
+            for (CheckBox checkBox : testItems) {
+                if (checkBox.isSelected()) {
+                    System.out.println("Selected: " + checkBox.getText());
+                    // Place your logic to run the test here.
+                }
+            }
         });
-        gridPane.add(runTestsButton, 0, 1, 2, 1); // Add to column=0, row=1, colspan=2, rowspan=1
 
-        // Test results label - placeholder
-        Label testResultsLabel = new Label("Test Results Will Appear Here");
-        gridPane.add(testResultsLabel, 0, 2, 2, 1); // Add to column=0, row=2, colspan=2, rowspan=1
+        gridPane.add(runTestsButton, 0, 1, 2, 1);
 
-        // Setting the scene
-        Scene scene = new Scene(gridPane, 500, 275); // Width: 500px, Height: 275px
+        Scene scene = new Scene(gridPane, 500, 300);
         applicationStage.setScene(scene);
-
-        // Show the stage
         applicationStage.show();
     }
 
     public static void main(String[] args) {
-        launch(args); // Launch application
+        launch(args);
     }
 }
